6f87049c1d219954b8b63663c1384ff518a75f56
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-19 16:01:02 +0500

    Add admin model

---
+++ server/src/resources/admin/admin.controller.js
@@ -0,0 +1,29 @@
+const model = require('./admin.model')
+
+const getAll = async (req, res) => {
+
+}
+
+const createOne = async (req, res) => {
+	
+}
+
+const getOne = async (req, res) => {
+	
+}
+
+const editOne = async (req, res) => {
+	
+}
+
+const deleteOne = async (req, res) => {
+	
+}
+
+module.exports = {
+	getAll,
+	createOne,
+	getOne,
+	editOne,
+	deleteOne
+}
---
+++ server/src/resources/admin/admin.route.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const controller = require('./admin.controller')
+
+const router = Router()
+
+// /admin/orders
+router
+	.route('/carts')
+	.get(controller.getAll)
+	.post(controller.createOne)
+
+// /admin/order/:id
+router
+	.route('/cart/:id')
+	.get(controller.getOne)
+	.put(controller.editOne)
+	.delete(controller.deleteOne)
