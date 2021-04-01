2f06ebb8e3aec694890124e43d6be9f805c6d593
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-01 16:18:26 +0500

    Select lang by callback

--- bot/src/helper.js
+++ bot/src/helper.js
@@ -1,9 +1,30 @@
 module.exports = {
 	getChatId (msg) {
-		return msg.chat.id
+		return msg?.chat?.id || msg?.message?.chat?.id
+	},
+
+	getName (msg) {
+		return msg?.from?.first_name
+	},
+
+	getLang (msg) {
+		return msg?.from?.language_code
+	},
+
+	getClientObj (msg) {
+		return {
+			tg_user_id: msg.from.id,
+			tg_first_name: msg.from.first_name,
+			tg_last_name: msg.from?.last_name || '',
+			tg_username: msg.from?.username || ''
+		}
 	},
 	
 	logStart () {
 		console.log('Bot started...')
+	},
+
+	debug (obj) {
+		return JSON.stringify(obj, null, 4)
 	},
 }
