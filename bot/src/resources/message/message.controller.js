8bf1a4e2ee72c4f285dda710505c3e33e6656a43
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-01 16:20:18 +0500

    Add bot message module

---
+++ bot/src/resources/message/message.controller.js
@@ -0,0 +1,11 @@
+const helper = require('./../../helper')
+
+const getAction = async (msg) => {
+
+	// bot.sendMessage(helper.getChatId(msg), 'working...')
+
+}
+
+module.exports = {
+	getAction,
+}
