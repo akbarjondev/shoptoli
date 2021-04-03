5d3f22a93ec8f3f13f09c9e8b981921569b61d34
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-03 14:18:48 +0500

    Add to helper getMsgId, getUserLang, getRegions, setRegion

--- bot/src/helper.js
+++ bot/src/helper.js
@@ -1,6 +1,13 @@
+const fetch = require('node-fetch')
+const CONFIG = require('./config/config')
+
 module.exports = {
 	getChatId (msg) {
 		return msg?.chat?.id || msg?.message?.chat?.id
+	},
+
+	getMsgId (msg) {
+		return msg?.message_id || msg?.message?.message_id
 	},
 
 	getName (msg) {
@@ -11,7 +18,14 @@ module.exports = {
 		return msg?.from?.language_code
 	},
 
-	getClientObj (msg) {
+	getUserLang: async (chatId) => {
+		const getUserLangRes = await fetch(`${CONFIG.SERVER_HOST}/bot/client/lang/${chatId}`)
+		const res = await getUserLangRes.json()
+
+		return res.data[0].language
+	},
+
+	getClientObj: (msg) => {
 		return {
 			tg_user_id: msg.from.id,
 			tg_first_name: msg.from.first_name,
@@ -20,11 +34,34 @@ module.exports = {
 		}
 	},
 	
-	logStart () {
+	logStart: () => {
 		console.log('Bot started...')
 	},
 
-	debug (obj) {
+	debug: (obj) => {
 		return JSON.stringify(obj, null, 4)
 	},
+
+	getRegions: async (language) => {
+		const getRegions = await fetch(`${CONFIG.SERVER_HOST}/bot/regions/${language}`)
+		const res = await getRegions.json()
+
+		return res.data
+	},
+
+	setRegion: async (msg, regionId) => {
+		const setRegion = await fetch(`${CONFIG.SERVER_HOST}/bot/regions`, {
+			method: 'put',
+			headers: {
+	      'Content-Type': 'application/json'
+	    },
+			body: JSON.stringify({
+				region_id: regionId,
+				tg_user_id: helper.getChatId(msg)
+			})
+		})
+		const res = await setRegion.json()
+
+		return res.data
+	}	
 }
