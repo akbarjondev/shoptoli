6d57d5ce56838b6b91ab940c346507484dfff73b
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-03 14:21:19 +0500

    Add button and menu texts

---
+++ bot/src/texts.js
@@ -0,0 +1,14 @@
+module.exports = {
+	askRegion: {
+		uz: 'Qaysi hududdan buyurtma berasiz? 🗺🏢🏡',
+		ru: 'С какого района вы заказываете? 🗺🏢🏡'
+	},
+	selectRegion: {
+		uz: 'Hudud tanlandi',
+		ru: 'Район выбран'
+	},
+	selectLang: {
+		uz: 'Til tanlandi',
+		ru: 'Язык выбран'
+	}
+}
