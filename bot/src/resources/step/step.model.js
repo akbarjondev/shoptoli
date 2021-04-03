c50f5647629bb022ae1fbf4128dcefb1c81b084a
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-03 14:23:00 +0500

    Add editStep

--- bot/src/resources/step/step.model.js
+++ bot/src/resources/step/step.model.js
@@ -28,6 +28,31 @@ const addStep = async (msg, step_name = 'start') => {
 
 }
 
+// edit step
+
+const editStep = async (msg, step_name) => {
+
+	try {
+
+		const editStep = await fetch(`${CONFIG.SERVER_HOST}/bot/step`, {
+			method: 'put',
+			headers: {
+	      'Content-Type': 'application/json'
+	    },
+			body: JSON.stringify({
+				step_name: step_name,
+				tg_user_id: helper.getChatId(msg)
+			})
+		})
+
+		return await editStep.json()
+
+	} catch(e) {
+		console.log(e)
+	}
+
+}
+
 const getStep = async (msg) => {
 
 	try {
@@ -45,4 +70,5 @@ const getStep = async (msg) => {
 module.exports = {
 	addStep,
 	getStep,
+	editStep,
 }
