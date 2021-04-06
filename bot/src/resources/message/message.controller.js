ffeea238b89c1dac7fa81c2bbac913fb22b0ca27
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-06 11:54:10 +0500

    Add bot: send catalog WIP

--- bot/src/resources/message/message.controller.js
+++ bot/src/resources/message/message.controller.js
@@ -1,8 +1,41 @@
+const fetch = require('node-fetch')
+const CONFIG = require('./../../config/config')
 const helper = require('./../../helper')
+const text = require('./../../texts')
 
 const getAction = async (msg) => {
 
-	// bot.sendMessage(helper.getChatId(msg), 'working...')
+	// get to know user's language
+	const userLang = await helper.getUserLang(helper.getChatId(msg))
+
+	// user ask catalog
+	if(msg.text === text.mainMenu.keyboard.order[userLang]) {
+
+		try {
+			
+			const getGeneralInfo = await fetch(`${CONFIG.SERVER_HOST}/bot/infos`)
+
+			const response = await getGeneralInfo.json()
+
+			const company = response.data[0].info_company_name
+			const link = response.data[0].info_catalog_link
+
+			bot.sendMessage(
+				helper.getChatId(msg), 
+				`
+					<b>${company.toUpperCase()}</b> <a href="${link}">ℹ️</a>\n\n<b>${text.sendCatalog[userLang]}</b>
+				`,
+				{
+					parse_mode: 'html'
+				}
+			)
+
+		} catch(e) {
+			console.log(e)
+		}
+
+	}
+
 
 }
 
