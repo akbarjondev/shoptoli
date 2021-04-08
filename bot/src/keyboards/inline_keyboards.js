af568dedc89ec7f27a571597b1e62c85a0af8420
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-04-08 12:37:45 +0500

    Add bot: inline keyboard for product quantity

---
+++ bot/src/keyboards/inline_keyboards.js
@@ -0,0 +1,17 @@
+module.exports = {
+	product_quantity: (cat_id) => {
+
+		return [
+			{ name: 1, id: 1, cat_id },
+			{ name: 2, id: 2, cat_id },
+			{ name: 3, id: 3, cat_id },
+			{ name: 4, id: 4, cat_id },
+			{ name: 5, id: 5, cat_id },
+			{ name: 6, id: 6, cat_id },
+			{ name: 7, id: 7, cat_id },
+			{ name: 8, id: 8, cat_id },
+			{ name: 9, id: 9, cat_id }
+		]
+
+	},
+}
