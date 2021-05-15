bddeaca2e1cd28054abfb63d4745fe03c8584f52
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-05-15 13:49:23 +0500

    Minor

--- server/src/libs/jwt/jwt.js
+++ server/src/libs/jwt/jwt.js
@@ -3,6 +3,6 @@ const jwt = require('jsonwebtoken')
 ;require('dotenv').config()
 
 module.exports = {
-	sign: (payload) => jwt.sign(payload, 'Ajwa_taomlari!', { expiresIn: '3h' }), // payload must be Object {foo: 'bar'}
+	sign: (payload) => jwt.sign(payload, 'Ajwa_taomlari!', { expiresIn: '8h' }), // payload must be Object {foo: 'bar'}
 	verify: (token) => jwt.verify(token, 'Ajwa_taomlari!')
 }
